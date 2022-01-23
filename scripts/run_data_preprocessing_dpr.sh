c8c7d5b3269f134bf97275a3190f8d2dbd2159bc
Danny Luo <dannyluozp@gmail.com>
2022-01-23 15:49:00 -0500

    new

--- scripts/run_data_preprocessing_dpr.sh
+++ scripts/run_data_preprocessing_dpr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-seg=$1 # token or structure
-domain=$2 # dmv va ssa or studentaid
-YOUR_DIR=../data # change it to your own local dir
+seg='token' # token or structure
+domain='dmv' # dmv va ssa or studentaid
+YOUR_DIR='../preprocess_dpr' # change it to your own local dir
 
 python data_preprocessor.py \
 --dataset_config_name multidoc2dial \
